35860d9b33cd7128458ca316a0d9f6855e9fd0e3
Sacettin Şahin <eng.sacettinsahin@gmail.com>
2025-01-08 15:50:37 +0300

    Main layout added to project.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,22 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 import { CartComponent } from './pages/cart/cart.component';
+import { MainComponent } from './layout/main/main.component';
 
 export const routes: Routes = [
-    {
-        path:'',
-        pathMatch:'full',
-        component: HomepageComponent
-    },
-    {
-        path:'cart',
-        component: CartComponent
-    }
+   {
+    path: '',
+    component: MainComponent,
+    children: [
+        {
+            path:'',
+            pathMatch:'full',
+            component: HomepageComponent
+        },
+        {
+            path:'cart',
+            component: CartComponent
+        }
+    ]
+   }
 ];
